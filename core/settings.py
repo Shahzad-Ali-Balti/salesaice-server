b2976645b0821a31ab461ba258cd35a9bdf1f882
shahzad_ali <shahzadalibalti216@gmail.com>
2025-07-25 00:16:38 +0500

    updated api

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-=9tgzd3ihuj%8il22ah=%kodb%ddz&3r*jb_du11w!)amcr#_4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aicegroup.pythonanywhere.com']
+ALLOWED_HOSTS = ['aicegroup.pythonanywhere.com','127.0.0.1',"localhost"]
 
 # Application definition
 INSTALLED_APPS = [
--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls')),  # this assumes app name is 'accounts'
+    path('api/accounts/', include('accounts.urls')),  # this assumes app name is 'accounts'
 ]
 
